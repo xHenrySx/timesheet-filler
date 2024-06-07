5657e754b8a972b1f53f3eb71a16260fb2437f2b
xHenrySx <enrisal2000@gmail.com>
2024-06-07 08:18:57 -0400

    Remove unused meta function

--- client/app/routes/_index.jsx
+++ client/app/routes/_index.jsx
@@ -1,15 +1,6 @@
 import ActivitieForm from '../components/activitieform';
 import { ActivitiesList } from '../components/activitieslist';
 import { useState } from 'react';
-export const meta = () => {
-  return [
-    { title: 'Timesheet Filler - Actividades' },
-    {
-      name: 'description',
-      content: 'A simple app to fill activities in Sodep',
-    },
-  ];
-};
 
 export default function Index() {
   const [refreshKey, setRefreshKey] = useState(0);
