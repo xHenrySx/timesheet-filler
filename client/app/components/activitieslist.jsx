1a26043fe02cd8cd51f9caea63a1402be027e376
xHenrySx <enrisal2000@gmail.com>
2024-06-12 09:14:42 -0400

    Add filter and loading state

--- client/app/components/activitieslist.jsx
+++ client/app/components/activitieslist.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { Column } from 'primereact/column';
 import { Card } from 'primereact/card';
 import { getActivities } from '../utils/activities';
@@ -7,10 +8,21 @@ import { getActivities } from '../utils/activities';
 export function ActivitiesList() {
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const filters = {
+    Fecha: {value: null, matchMode: FilterMatchMode.EQUALS},
+    Descripcion: {value: null, matchMode: FilterMatchMode.CONTAINS},
+    Duracion: {value: null, matchMode: FilterMatchMode.EQUALS},
+    'Jira Ticket': {value: null, matchMode: FilterMatchMode.EQUALS},
+    'Jira Cliente Ticket': {value: null, matchMode: FilterMatchMode.EQUALS},
+  };
+
 
   useEffect(() => {
     async function fetchData() {
       setData(await getActivities());
+      setLoading(false);
     }
 
 
@@ -36,12 +48,15 @@ export function ActivitiesList() {
         tableStyle={{
           minWidth: '50rem',
         }}
+        loading={loading}
         paginator
         rows={5}
         rowsPerPageOptions={[5, 10, 15]}
         paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
         currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} actividades"
         removableSort
+        emptyMessage="No hay actividades."
+        filters={filters}
       >
         {columns.map(column => (
           <Column
@@ -50,6 +65,7 @@ export function ActivitiesList() {
             header={column.header}
             style={{ minWidth: '25%' }}
             sortable
+            filter
           />
         ))}
       </DataTable>
