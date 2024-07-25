00a501aea135c990020fab784c122f5a1180f6d9
xHenrySx <enrisal2000@gmail.com>
2024-07-24 20:44:50 -0400

    Añade funciones utiles de fechas

--- client/app/utils/date.js
+++ client/app/utils/date.js
@@ -1,6 +1,47 @@
-export function formatDate(date) {
-  if (typeof(date) === 'string') {
+const options = {
+  timezone: 'UTC',
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+};
+
+const pattern = /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/; // dd/mm/aaaa
+/**
+ * Formatea un objeto date
+ * @param {Date} date
+ * @returns String
+ */
+export function dateToString(date, locale) {
+  if (typeof date === 'string' && pattern.test(date)) {
+    date = date.split('/').reverse().join('/');
+  }
+
+  if (typeof date === 'string') {
     date = new Date(date);
   }
-  return date.toLocaleDateString('es-PY');
+
+  const defaultLocale = locale || 'es-PY';
+
+  return date.toLocaleDateString(defaultLocale, options);
+}
+
+/**
+ * Convierte un string a fecha
+ * @param {String} date
+ * @returns {Date}
+ */
+export function stringToDate(date) {
+  if (typeof date !== 'string') {
+    throw new Error('Parameter date its not a string.');
+  }
+
+  if (typeof date === 'string' && pattern.test(date)) {
+    date = date.split('/').reverse().join('/');
+  }
+
+  if (typeof date === 'string') {
+    date = new Date(date);
+  }
+
+  return date;
 }
